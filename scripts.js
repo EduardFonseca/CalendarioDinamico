1887ba0b47b1e38149153dab4d9c4fa85c13ddca
Eduardo Fonseca <duduardo3000@gmail.com>
2025-04-05 22:56:45 +0300

    site parcialmente finalizado

--- scripts.js
+++ scripts.js
@@ -10,6 +10,10 @@ function countUpTime() {
         document.getElementById("hours").textContent = "00";
         document.getElementById("minutes").textContent = "00";
         document.getElementById("seconds").textContent = "00";
+        document.getElementById("miojo").textContent = "00";
+        document.getElementById("ratatouille").textContent = "00";
+        document.getElementById("coisaBoa").textContent = "00";
+        document.getElementById("voo").textContent = "00";
         return;
       }
   
@@ -17,11 +21,21 @@ function countUpTime() {
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      const miojo = Math.floor((distance/(1000 * 60 * 3))) // quantidade de miojos que se pode fazer nesse tempo
+      const ratatouille = Math.floor((distance/(1000 * 60 * 111))) //quantidade de vezes que poderiamos assitir ratatoullie
+      const coisaBoa = Math.floor((distance/(1000 * 140))) // quantidades de vezes que poderiamos escutar coisa boa
+      const voo = Math.floor((distance/(1000 * 60 * 60 * 11.25))) // Numero de voos Rio Orlando
   
-      document.getElementById("days").textContent = String(days).padStart(2, '0');
-      document.getElementById("hours").textContent = String(hours).padStart(2, '0');
-      document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
-      document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+      document.getElementById("days").textContent = String(days + " dias ").padStart(2, '0');
+      document.getElementById("hours").textContent = String(hours + " horas ").padStart(2, '0');
+      document.getElementById("minutes").textContent = String(minutes + " minutos ").padStart(2, '0');
+      document.getElementById("seconds").textContent = String(seconds + " segundos ").padStart(2, '0');
+      document.getElementById("miojo").textContent = String("Feito " + miojo + " miojos.");
+      document.getElementById("ratatouille").textContent = String("Assitido Ratatouille " + ratatouille + " vezes.");
+      document.getElementById("coisaBoa").textContent = String("Escutado "+ coisaBoa + " Coisa Boa do Lagum.");
+      document.getElementById("voo").textContent = String("Voado " + voo + " vezes entre Rio e Orlando.");
+
     }, 1000);
   }
   
